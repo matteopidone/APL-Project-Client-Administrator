99b4dd7713ba6a92d3b8458dcc2fcbb4c08d6397
Tomas Prifti <tomas.prifti@ctmobi.it>
2023-03-16 19:28:51 +0100

    feat(homewindow): implentazione interfaccia di base per la home
    Implementazione di bottoni laterali per la gestione di funzionalità future. Implementazione di
    tabella a griglia per la visualizzazione delle richieste ferie dei dipendenti

--- classes/HomeWindow.py
+++ classes/HomeWindow.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QWidget
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QPushButton, QGridLayout, QTableWidget, QTableWidgetItem, QHBoxLayout
 from PySide6.QtGui import QFont
 
 class HomeWindow(QWidget):
@@ -12,11 +12,72 @@ class HomeWindow(QWidget):
 
 		# Definisco lo stile
 		self.setWindowTitle("Home")
-		self.resize(800, 400)
-		self.setFont(QFont("Arial", 32))
+		self.resize(1600, 800)
+		self.setFont(QFont("Arial", 24))
 
 		self.create_interface()
 
 	# Funzione per la generazione dell'interfaccia grafica
 	def create_interface(self):
-		pass
+		# Creo il layout per i bottoni
+		left_layout = QVBoxLayout()
+
+		# Creo i bottoni e li aggiungo al layout di sinistra
+		button1 = QPushButton("Elenco dipendenti")
+		button2 = QPushButton("Aggiungi dipendente")
+		button3 = QPushButton("Esci")
+		left_layout.addWidget(button1)
+		left_layout.addWidget(button2)
+		left_layout.addWidget(button3)
+
+		# Creo il layout per la tabella sulla destra
+		right_layout = QGridLayout()
+
+		user1 = {'name':'tomas', 'surname':'prifti', 'date':'21 Marzo 2023'}
+		user2 = {'name':'matteo', 'surname':'pidone', 'date':'22 Marzo 2023'}
+		data = [user1, user2]
+
+
+		# Creo la tabella
+		table = QTableWidget(len(data), len(data[0])+1)
+		l = list(data[0].keys())
+		l.append("Azioni")
+		table.setHorizontalHeaderLabels(l)
+
+		# Popolo la tabella con le richieste di ferie
+		i = 0
+		for row in data:
+			j=0
+			for key, value in row.items():
+				item = QTableWidgetItem(value)
+				table.setItem(i, j, item)
+				table.setColumnWidth(j, 300)
+				if j == len(row)-1:
+					# Creo i bottoni
+					accept_button = QPushButton("Accetta")
+					reject_button = QPushButton("Rifiuta")
+					# Imposto il colore di sfondo dei bottoni
+					accept_button.setStyleSheet("background-color: green")
+					reject_button.setStyleSheet("background-color: red")
+					# Creo un contenitore e aggiungo i bottoni al layout a griglia
+					widget = QWidget()
+					layout = QHBoxLayout()
+					layout.addWidget(accept_button)
+					layout.addWidget(reject_button)
+					widget.setLayout(layout)
+					table.setCellWidget(i, j+1, widget)
+					table.setColumnWidth(j+1, 300)
+				j += 1
+			table.setRowHeight(i, 100)
+			i += 1
+
+		# Aggiungo la tabella al layout di destra
+		right_layout.addWidget(table)
+
+		# Creo un layout principale per contenere i due layout precedenti
+		main_layout = QHBoxLayout()
+		main_layout.addLayout(left_layout)
+		main_layout.addLayout(right_layout)
+
+		# Imposto il layout della finestra
+		self.setLayout(main_layout)
