ad353318158798068744e027026d7c48a76efb01
Tomas Prifti <tomas.prifti@ctmobi.it>
2023-03-16 22:03:23 +0100

    feat(homewindow): implementazione funzioni per la gestione dei layout presenti nella finestra
    Implementazione funzione per il recupero delle richieste di ferie dei dipendenti (da validare), e
    funzione per la risposta alla specifica richiesta (da validare). Aggiunti nuovi url nel file .env
    (da validare con il server)

--- classes/HomeWindow.py
+++ classes/HomeWindow.py
@@ -1,5 +1,7 @@
 from PySide6.QtWidgets import QWidget, QVBoxLayout, QPushButton, QGridLayout, QTableWidget, QTableWidgetItem, QHBoxLayout
 from PySide6.QtGui import QFont
+import requests
+import os
 
 class HomeWindow(QWidget):
 
@@ -19,24 +21,51 @@ class HomeWindow(QWidget):
 
 	# Funzione per la generazione dell'interfaccia grafica
 	def create_interface(self):
-		# Creo il layout per i bottoni
+		# Creo il layout per i bottoni sulla sinistra
 		left_layout = QVBoxLayout()
+		self.create_left_layout(left_layout)
 
-		# Creo i bottoni e li aggiungo al layout di sinistra
+		# Creo il layout per la tabella sulla destra
+		right_layout = QGridLayout()
+		self.create_right_layout(right_layout)
+
+		# Creo un layout principale per contenere i due layout precedenti
+		main_layout = QHBoxLayout()
+		main_layout.addLayout(left_layout)
+		main_layout.addLayout(right_layout)
+
+		# Imposto il layout della finestra
+		self.setLayout(main_layout)
+
+	# Funzione per la creazione del layout di sinistra
+	def create_left_layout(self, left_layout):
+		# Creo i bottoni
 		button1 = QPushButton("Elenco dipendenti")
 		button2 = QPushButton("Aggiungi dipendente")
 		button3 = QPushButton("Esci")
+
+		font = QFont("Arial", 22)
+		button1.setFont(font)
+		button1.setFixedSize(350, 50)
+		button2.setFont(font)
+		button2.setFixedSize(350, 50)
+		button3.setFont(font)
+		button3.setFixedSize(350, 50)
+		button3.setStyleSheet("background-color: red")
+
+		# Li aggiungo al layout di sinistra
 		left_layout.addWidget(button1)
 		left_layout.addWidget(button2)
 		left_layout.addWidget(button3)
 
-		# Creo il layout per la tabella sulla destra
-		right_layout = QGridLayout()
+	# Funzione per la creazione del layout di destra
+	def create_right_layout(self, right_layout):
+
+		data = self.getAllUsersHolidays()
 
 		user1 = {'name':'tomas', 'surname':'prifti', 'date':'21 Marzo 2023'}
 		user2 = {'name':'matteo', 'surname':'pidone', 'date':'22 Marzo 2023'}
 		data = [user1, user2]
-
 
 		# Creo la tabella
 		table = QTableWidget(len(data), len(data[0])+1)
@@ -48,7 +77,7 @@ class HomeWindow(QWidget):
 		i = 0
 		for row in data:
 			j=0
-			for key, value in row.items():
+			for value in row.values():
 				item = QTableWidgetItem(value)
 				table.setItem(i, j, item)
 				table.setColumnWidth(j, 300)
@@ -56,9 +85,14 @@ class HomeWindow(QWidget):
 					# Creo i bottoni
 					accept_button = QPushButton("Accetta")
 					reject_button = QPushButton("Rifiuta")
-					# Imposto il colore di sfondo dei bottoni
+					# Imposto lo stile dei bottoni
+					accept_button.setFixedSize(100, 50)
 					accept_button.setStyleSheet("background-color: green")
+					reject_button.setFixedSize(100, 50)
 					reject_button.setStyleSheet("background-color: red")
+					# Imposto la funzione da invocare al click
+					accept_button.clicked.connect(lambda: self.replyUserRequest(row, 1))
+					reject_button.clicked.connect(lambda: self.replyUserRequest(row, 2))
 					# Creo un contenitore e aggiungo i bottoni al layout a griglia
 					widget = QWidget()
 					layout = QHBoxLayout()
@@ -74,10 +108,37 @@ class HomeWindow(QWidget):
 		# Aggiungo la tabella al layout di destra
 		right_layout.addWidget(table)
 
-		# Creo un layout principale per contenere i due layout precedenti
-		main_layout = QHBoxLayout()
-		main_layout.addLayout(left_layout)
-		main_layout.addLayout(right_layout)
+	# Funzione per il recupero di tutte le richieste di ferie dei dipendenti
+	def getAllUsersHolidays(self):
+		url = os.environ.get('URL_GET_ALL_USERS_HOLIDAYS')
 
-		# Imposto il layout della finestra
-		self.setLayout(main_layout)
+		try:
+			response = requests.get(url)
+
+			if response.status_code == 200:
+				return response.json()
+			else:
+				return
+		except requests.exceptions.RequestException:
+			# Gestione dell'eccezione
+			print("Impossibile connettersi al server.")
+
+	# Funzione per la risposta alla richiesta di ferie
+	def replyUserRequest(self, row, choice):
+		url = os.environ.get('URL_REPLY_USER_REQUEST')
+		row['choice'] = choice
+
+		print(row)
+
+		try:
+			response = requests.post(url=url, json=row)
+
+			if response.status_code == 200:
+				# to define
+				return
+			else:
+				# to define
+				return
+		except requests.exceptions.RequestException:
+			# Gestione dell'eccezione
+			print("Impossibile connettersi al server.")
